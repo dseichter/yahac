f00c99d89cc262600f360bd237cf6108f03626cb
Daniel Seichter <daniel.seichter@dseichter.de>
2025-10-10 21:17:39 +0200

    show update message, if version is greater

--- src/helper.py
+++ src/helper.py
@@ -17,9 +17,10 @@ import urllib3
 import socket
 import json
 import logging
+from packaging import version
 
 VERSION = "v0.3.0"
-UPDATEURL = 'https://api.github.com/repos/dseichter/yahac/releases/latest'
+UPDATEURL = 'https://api.github.com/repos/dseichter/yahac/releases'
 RELEASES = 'https://github.com/dseichter/yahac/releases'
 WEBSITE = 'https://dseichter.github.io/yahac/'
 NAME = 'yahac'
@@ -31,9 +32,13 @@ def check_for_new_release():
     try:
         http = urllib3.PoolManager()
         r = http.request('GET', UPDATEURL)
-        data = json.loads(r.data.decode('utf-8'))
-        latest_version = data['tag_name']
-        return latest_version != VERSION
+        releases = json.loads(r.data.decode('utf-8'))
+        # Find the latest stable (not prerelease) release
+        for release in releases:
+            if not release.get('prerelease', False):
+                latest_version = release.get('tag_name')
+                return version.parse(latest_version) > version.parse(VERSION)
+        return False  # No stable release found
     except Exception as e:
         logging.error(f"Error checking for new release: {e}")
         return False
